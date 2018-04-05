8dec402717f5eda39adad167d02acf9aa9f76ece
David Smekal <David.Smekal@hotmail.com>
2018-04-04 21:05:52 -0700

    Added user statistics into database

---
+++ Assets/databasePlaying.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// everytime a user navigates to the home screen, it will add the date and time they entered.
+// I'm doing this to see if users are actually playing my game or not.
+public class databasePlaying : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        StartCoroutine(Upload());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // this will update the user's score
+    IEnumerator Upload()
+    {
+        // gets the users unique deviceId
+        String uniqueSystemIde = SystemInfo.deviceUniqueIdentifier;
+
+        WWWForm form = new WWWForm();
+
+        form.AddField("username", PlayerPrefs.GetString("Username"));
+        form.AddField("deviceId", uniqueSystemIde);
+
+
+        UnityWebRequest www = UnityWebRequest.Post("http://107.170.227.172/mainMenuDatabase.php", form);
+        yield return www.Send();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            Debug.Log("ERROR CAN'T UPLOAD");
+
+        }
+        else
+
+        {
+            Debug.Log("Form upload complete!");
+
+
+        }
+
+
+
+    }
+
+}
