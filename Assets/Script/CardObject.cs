32fa1d59648c58a47931677b4ad5668764c0037d
David Smekal <David.Smekal@hotmail.com>
2018-11-12 02:39:27 -0800

    Created a deck object and cleaned up some code

--- Assets/Script/CardObject.cs
+++ Assets/Script/CardObject.cs
@@ -1,10 +1,81 @@
-﻿namespace CardObjects {
+﻿using System.Collections.Generic;
+
+namespace CardObjects {
 
     public class CardObject
     {
 
         public int value { get; set; }
         public string face { get; set; }
+
+    }
+
+    // type. 1 = total. 2 = contains ace. 3 = pair.
+    public class CreateDeck
+    {
+        public static List<CardObject> createDeck()
+        {
+            List<CardObject> deckList = new List<CardObject>
+        {
+
+            new CardObject() {value = 1, face = "Ace"},
+            new CardObject() {value = 1, face = "Ace"},
+            new CardObject() {value = 1, face = "Ace"},
+            new CardObject() {value = 1, face = "Ace"},
+            new CardObject() {value = 2, face = "2"},
+            new CardObject() {value = 2, face = "2"},
+            new CardObject() {value = 2, face = "2"},
+            new CardObject() {value = 2, face = "2"},
+            new CardObject() {value = 3, face = "3"},
+            new CardObject() {value = 3, face = "3"},
+            new CardObject() {value = 3, face = "3"},
+            new CardObject() {value = 3, face = "3"},
+            new CardObject() {value = 4, face = "4"},
+            new CardObject() {value = 4, face = "4"},
+            new CardObject() {value = 4, face = "4"},
+            new CardObject() {value = 4, face = "4"},
+            new CardObject() {value = 5, face = "5"},
+            new CardObject() {value = 5, face = "5"},
+            new CardObject() {value = 5, face = "5"},
+            new CardObject() {value = 5, face = "5"},
+            new CardObject() {value = 6, face = "6"},
+            new CardObject() {value = 6, face = "6"},
+            new CardObject() {value = 6, face = "6"},
+            new CardObject() {value = 6, face = "6"},
+            new CardObject() {value = 7, face = "7"},
+            new CardObject() {value = 7, face = "7"},
+            new CardObject() {value = 7, face = "7"},
+            new CardObject() {value = 7, face = "7"},
+            new CardObject() {value = 8, face = "8"},
+            new CardObject() {value = 8, face = "8"},
+            new CardObject() {value = 8, face = "8"},
+            new CardObject() {value = 8, face = "8"},
+            new CardObject() {value = 9, face = "9"},
+            new CardObject() {value = 9, face = "9"},
+            new CardObject() {value = 9, face = "9"},
+            new CardObject() {value = 9, face = "9"},
+            new CardObject() {value = 10, face = "10"},
+            new CardObject() {value = 10, face = "10"},
+            new CardObject() {value = 10, face = "10"},
+            new CardObject() {value = 10, face = "10"},
+            new CardObject() {value = 10, face = "Jack"},
+            new CardObject() {value = 10, face = "Jack"},
+            new CardObject() {value = 10, face = "Jack"},
+            new CardObject() {value = 10, face = "Jack"},
+            new CardObject() {value = 10, face = "Queen"},
+            new CardObject() {value = 10, face = "Queen"},
+            new CardObject() {value = 10, face = "Queen"},
+            new CardObject() {value = 10, face = "Queen"},
+            new CardObject() {value = 10, face = "King"},
+            new CardObject() {value = 10, face = "King"},
+            new CardObject() {value = 10, face = "King"},
+            new CardObject() {value = 10, face = "King"},
+        };
+
+            return deckList;
+
+        }
+
 
     }
 
--- Assets/Script/trainMode.cs
+++ Assets/Script/trainMode.cs
@@ -191,63 +191,8 @@ public class trainMode : MonoBehaviour
         //finds the skin stored in player preference and uses it to set the cardback
         skin = PlayerPrefs.GetString("Skin");
 
-        // type. 1 = total. 2 = contains ace. 3 = pair.
-        List<CardObject> deckList = new List<CardObject>
-        {
-            new CardObject() {value = 1, face = "Ace"},
-            new CardObject() {value = 1, face = "Ace"},
-            new CardObject() {value = 1, face = "Ace"},
-            new CardObject() {value = 1, face = "Ace"},
-            new CardObject() {value = 2, face = "2"},
-            new CardObject() {value = 2, face = "2"},
-            new CardObject() {value = 2, face = "2"},
-            new CardObject() {value = 2, face = "2"},
-            new CardObject() {value = 3, face = "3"},
-            new CardObject() {value = 3, face = "3"},
-            new CardObject() {value = 3, face = "3"},
-            new CardObject() {value = 3, face = "3"},
-            new CardObject() {value = 4, face = "4"},
-            new CardObject() {value = 4, face = "4"},
-            new CardObject() {value = 4, face = "4"},
-            new CardObject() {value = 4, face = "4"},
-            new CardObject() {value = 5, face = "5"},
-            new CardObject() {value = 5, face = "5"},
-            new CardObject() {value = 5, face = "5"},
-            new CardObject() {value = 5, face = "5"},
-            new CardObject() {value = 6, face = "6"},
-            new CardObject() {value = 6, face = "6"},
-            new CardObject() {value = 6, face = "6"},
-            new CardObject() {value = 6, face = "6"},
-            new CardObject() {value = 7, face = "7"},
-            new CardObject() {value = 7, face = "7"},
-            new CardObject() {value = 7, face = "7"},
-            new CardObject() {value = 7, face = "7"},
-            new CardObject() {value = 8, face = "8"},
-            new CardObject() {value = 8, face = "8"},
-            new CardObject() {value = 8, face = "8"},
-            new CardObject() {value = 8, face = "8"},
-            new CardObject() {value = 9, face = "9"},
-            new CardObject() {value = 9, face = "9"},
-            new CardObject() {value = 9, face = "9"},
-            new CardObject() {value = 9, face = "9"},
-            new CardObject() {value = 10, face = "10"},
-            new CardObject() {value = 10, face = "10"},
-            new CardObject() {value = 10, face = "10"},
-            new CardObject() {value = 10, face = "10"},
-            new CardObject() {value = 10, face = "Jack"},
-            new CardObject() {value = 10, face = "Jack"},
-            new CardObject() {value = 10, face = "Jack"},
-            new CardObject() {value = 10, face = "Jack"},
-            new CardObject() {value = 10, face = "Queen"},
-            new CardObject() {value = 10, face = "Queen"},
-            new CardObject() {value = 10, face = "Queen"},
-            new CardObject() {value = 10, face = "Queen"},
-            new CardObject() {value = 10, face = "King"},
-            new CardObject() {value = 10, face = "King"},
-            new CardObject() {value = 10, face = "King"},
-            new CardObject() {value = 10, face = "King"},
-        };
 
+       List <CardObject> deckList = CardObjects.CreateDeck.createDeck();
 
 
         //populating the deck array
